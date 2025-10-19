3a0043568cd0cf718e1ad5af3e7734fbf0c5821a
jhsommer <129003263+jhsommer@users.noreply.github.com>
2025-10-19 17:57:51 +0200

    Update GameManager.cs
    Forgot to inlcude this file in the last input commit

--- Snake/GameManager.cs
+++ Snake/GameManager.cs
@@ -2,13 +2,42 @@ namespace Snake;
 
 public class GameManager
 {
+    private static GameManager instance;
+    public static GameManager Instance => instance ??= new GameManager();
+    private bool _wasInitialized;
+
+    private Snake _snake;
+    private Controller _controller;
+    private Fruit[] _fruits;
+
+    private GameManager()
+    { 
+        _wasInitialized =  false;
+        
+        _snake = Snake.Instance;
+        _controller =  Controller.Instance;
+        
+        _fruits =  new Fruit[20];
+    }
+
+    public void MovePlayer(Directions direction)
+    {
+        _snake.ChangeDirection(direction);
+    }
+    
    // private Snake _player =  new Snake();
-   private Controller _controller =  new Controller();
-   private Fruit []  _fruits =  new Fruit[20];
 
     public void Start()
     {
+        if (_wasInitialized)
+            return;
+        
+        _controller.SetPawn(_snake);
         _controller.Initialize();
+        
+        _snake.Initialize();
+        
+        _wasInitialized =  true;
     }
 
    public void Update()
