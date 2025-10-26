ca206d3b2ad84f77c359d47e4ecef668ffcf1307
jhsommer <129003263+jhsommer@users.noreply.github.com>
2025-10-26 11:28:09 +0100

    Update GameManager.cs
    Doesn't have a instance of snake anymore. Has the only instance of the Controller. Deleted Updated as it is not used.

--- Snake/GameManager.cs
+++ Snake/GameManager.cs
@@ -5,16 +5,14 @@ public class GameManager
     private static GameManager instance;
     public static GameManager Instance => instance ??= new GameManager();
     private bool _wasInitialized;
-
-    private Snake _snake;
-    private Controller _controller;
+    
+    internal Controller _controller;
     private Fruit[] _fruits;
 
     private GameManager()
     { 
         _wasInitialized =  false;
         
-        _snake = Snake.Instance;
         _controller =  Controller.Instance;
         
         _fruits =  new Fruit[20];
@@ -22,26 +20,18 @@ public class GameManager
 
     public void MovePlayer(Directions direction)
     {
-        _snake.ChangeDirection(direction);
+        _controller.Pawn.ChangeDirection(direction);
     }
     
-   // private Snake _player =  new Snake();
-
+   
     public void Start()
     {
         if (_wasInitialized)
             return;
         
-        _controller.SetPawn(_snake);
         _controller.Initialize();
-        
-        _snake.Initialize();
         
         _wasInitialized =  true;
     }
-
-   public void Update()
-    {
-        
-    }
+    
 }
