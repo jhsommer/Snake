3aa3a214f12f4c76d757b1c8dfe375fc2da89844
jhsommer <129003263+jhsommer@users.noreply.github.com>
2025-10-26 11:30:44 +0100

    Update Snake.cs
    Deletd Constructor. Deleted SetHeadPositionX -Y as it is not needed.

--- Snake/Snake.cs
+++ Snake/Snake.cs
@@ -2,9 +2,21 @@ namespace Snake;
 
 public class Snake
 {
-    private int _headPositionX =  0;
-    private int _headPositionY = 0;
-    //private Vector2 _tailPosition =  new Vector2(0, 0);
+    private int _headPositionX =  1;
+    private int _headPositionY = 1;
+    private Directions _currentDirection = Directions.Start;
+    private Controller _controller = null!;
+    
+    public void ChangeDirection(Directions direction)
+    {
+        _currentDirection = direction;
+        Move();
+    }
+    
+    public void Initialize()
+    {
+        SetPlayerStart();
+    }
 
    public int GetHeadPositionX()
     {
@@ -16,19 +28,38 @@ public class Snake
         return _headPositionY;
     }
 
-    public void SetHeadPositionX(int newHeadPositionX)
+    void PossessedBy(Controller newController)
     {
-        _headPositionX = newHeadPositionX;
+        _controller = newController;
+    }
+    
+    public void Move()
+    {
+        switch (_currentDirection)
+        {
+            case Directions.Up:
+                _headPositionY--;
+                break;
+            
+            case Directions.Down:
+                _headPositionY++;
+                break;
+            
+            case Directions.Left:
+                _headPositionX--;
+                break;
+            
+            case Directions.Right:
+                _headPositionX++;
+                break;
+        }
     }
 
-    public void SetHeadPositionY(int newHeadPositionY)
+    public void SetPlayerStart()
     {
-        _headPositionY = newHeadPositionY;
+        _headPositionX = Program.Width / 2;
+        _headPositionY = Program.Height / 2;
     }
 
-    public void Initialize()
-    {
-        _headPositionX = 20;
-        _headPositionY = 20;
-    }
+
 }
